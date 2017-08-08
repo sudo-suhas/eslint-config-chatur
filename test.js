c5e9555ac4e67b5494d196a5e1529b453d38d405
Suhas Karanth <sudo.suhas@gmail.com>
2017-08-08 09:27:52 +0530

    feat: add eslint configuration for test env
    Integrate dependency, optional plugins and override relevant rules. Dependency plugins -
    eslint-comments, node, unicorn. Optional plugins - promise, lodash, prettier, jest, ava

---
+++ test.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const { isInstalled, generateConfig } = require('./lib/util');
+
+const rules = {
+    /********************
+     * Strict Mode
+     *******************/
+    // http://eslint.org/docs/rules/strict
+    // require or disallow strict mode directives
+    strict: 'error',
+
+    /********************
+     * Node.js and CommonJS
+     *******************/
+    // http://eslint.org/docs/rules/no-buffer-constructor
+    // disallow use of the `Buffer()` constructor
+    'no-buffer-constructor': 'error',
+
+    // http://eslint.org/docs/rules/no-path-concat
+    // disallow string concatenation with `__dirname` and `__filename`
+    'no-path-concat': 'error',
+
+    // http://eslint.org/docs/rules/no-process-env
+    // disallow the use of `process.env`
+    'no-process-env': 'error',
+
+    // disallow the use of `process.exit()`
+    // http://eslint.org/docs/rules/no-process-exit
+    'no-process-exit': 'error'
+};
+
+const avaInstalled = isInstalled('ava');
+
+if (
+    isInstalled(`eslint-plugin-node`) &&
+    (avaInstalled || isInstalled(`jest`))
+) {
+    rules['node/no-unsupported-features'] = 'off';
+}
+
+if (isInstalled('lodash') && avaInstalled) {
+    rules['id-length'] = ['warn', { exceptions: ['t', '_'] }];
+}
+
+module.exports = generateConfig(
+    {
+        env: {
+            es6: true,
+            node: true,
+            builtin: true
+        },
+        rules
+    },
+    ['eslint-comments', 'node', 'unicorn'],
+    ['promise', 'lodash', 'prettier', 'jest', 'ava']
+);
